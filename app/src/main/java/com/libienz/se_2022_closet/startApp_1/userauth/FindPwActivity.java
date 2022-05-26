6417cba231411a07fbae81f9b5371e4554d406ef
Lkh-Dev <Leekh10195@gmail.com>
2022-05-27 04:28:35 +0900

    updateuser Utility almost done

--- app/src/main/java/com/libienz/se_2022_closet/startApp_1/userauth/FindPwActivity.java
+++ app/src/main/java/com/libienz/se_2022_closet/startApp_1/userauth/FindPwActivity.java
@@ -9,6 +9,8 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -21,6 +23,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.libienz.se_2022_closet.R;
 import com.libienz.se_2022_closet.databinding.ActivityFindPwBinding;
+import com.libienz.se_2022_closet.startApp_1.util.UpdateUserUtility;
 
 public class FindPwActivity extends AppCompatActivity {
 
@@ -30,76 +33,24 @@ public class FindPwActivity extends AppCompatActivity {
     private DatabaseReference userRef = database.getReference("user");
 
 
-    public void passwordReset() {
-
-        String inputEmail = binding.enterEmailToFindPw.getText().toString();
-        if (inputEmail.equals("")) {
-            binding.notifyEmailRes.setText("이메일 아이디를 입력해주세요!");
-            binding.notifyEmailRes.setTextColor(Color.parseColor("#B30000"));
-            return;
-        }
-        Log.d("toFind",inputEmail);
-        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                for (DataSnapshot userSnaposhot : snapshot.getChildren()) {
-
-                    String dbItrEmail = userSnaposhot.child("email").getValue().toString();
-
-                    Log.d("toFind",dbItrEmail);
-
-                    if(inputEmail.equals(dbItrEmail)) { //db에서 긁어온 메일과 입력 메일이 같을 때때
-                        //String password = userSnaposhot.child("password").getValue().toString();
-
-                        String email = binding.enterEmailToFindPw.getText().toString();
-                        auth.setLanguageCode("ko");
-                        auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<Void> task) {
-                                        if (task.isSuccessful()) {
-                                            binding.notifyEmailRes.setText("입력하신 이메일로 인증링크를 전송했습니다. 링크를 클릭하여 새로운 패스워드를 설정해주세요!");
-                                            binding.notifyEmailRes.setTextColor(Color.parseColor("#008000"));
-                                            Log.d("sent", "Email sent.");
-                                        }
-                                        else {
-                                            binding.notifyEmailRes.setText("입력하신 이메일로 인증링크를 전송하는데에 실패했습니다.");
-                                            binding.notifyEmailRes.setTextColor(Color.parseColor("#B30000"));
-                                        }
-                                    }
-                                });
-                        return;
-                    }
-                }
-                binding.notifyEmailRes.setText("회원가입되어 있지 않은 아이디입니다!");
-                binding.notifyEmailRes.setTextColor(Color.parseColor("#B30000"));
-                return;
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                // 디비를 가져오던중 에러 발생 시
-                //Log.e("MainActivity", String.valueOf(databaseError.toException())); // 에러문 출력
-            }
-        });
-
-
-    }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_find_pw);
+
         auth = FirebaseAuth.getInstance();
         FirebaseUser user = auth.getCurrentUser();
 
         binding.findPwBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                passwordReset();
+                EditText edt = binding.enterEmailToFindPw;
+                TextView tv = binding.notifyEmailRes;
+                UpdateUserUtility.passwordReset(edt,tv);
             }
         });
+
         binding.cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
