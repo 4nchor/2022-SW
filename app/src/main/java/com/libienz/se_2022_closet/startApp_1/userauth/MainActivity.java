38aafd2a862b5cf1243b9044044d89b29aa3cf57
Kim SeulA <seulah0629@gmail.com>
2022-05-30 02:47:48 +0900

    MainActivity 수정

--- app/src/main/java/com/libienz/se_2022_closet/startApp_1/userauth/MainActivity.java
+++ app/src/main/java/com/libienz/se_2022_closet/startApp_1/userauth/MainActivity.java
@@ -1,6 +1,8 @@
 package com.libienz.se_2022_closet.startApp_1.userauth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,11 +11,17 @@ import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.libienz.se_2022_closet.R;
+import com.libienz.se_2022_closet.startApp_1.fragments.addClothesFrag;
+import com.libienz.se_2022_closet.startApp_1.fragments.readClothesFrag;
 import com.libienz.se_2022_closet.startApp_1.ootd.OOTDActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction transaction;
+    private addClothesFrag addClothesFrag;
+    private readClothesFrag readClothesFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         auth = FirebaseAuth.getInstance();
+        fragmentManager = getSupportFragmentManager();
+
+        addClothesFrag = new addClothesFrag();
+        readClothesFrag = new readClothesFrag();
 
         Button logout_btn = (Button) findViewById(R.id.logout_btn);
         logout_btn.setOnClickListener(new View.OnClickListener() {
@@ -40,5 +52,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button addClothes_btn = (Button) findViewById(R.id.addClothes_btn);
+        addClothes_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.addClothes_fg, addClothesFrag).commit();
+            }
+        });
+
+        Button readClothes_btn = (Button) findViewById(R.id.readClothes_btn);
+        readClothes_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.readClothes_fg, readClothesFrag).commit();
+            }
+        });
+
     }
 }
