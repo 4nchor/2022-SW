6cc5928700ab06b3949c40f3981970cb61a0bc31
yejiniee <02doyoung01@naver.com>
2022-06-01 23:07:04 +0900

    add temperature

---
+++ app/src/main/java/com/libienz/se_2022_closet/startApp_1/util/WeatherModel.java
@@ -0,0 +1,14 @@
+package com.libienz.se_2022_closet.startApp_1.util;
+
+public class WeatherModel {
+    double temp = 0.0;  //온도
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+
+}
