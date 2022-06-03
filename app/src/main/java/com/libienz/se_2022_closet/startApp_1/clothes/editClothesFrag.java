aafd1c119f330b87788350b4f502e5dbe1d629f2
Jeong Chami <lasilla20@naver.com>
2022-06-03 18:55:27 +0900

    edit HashTag ~ing

--- app/src/main/java/com/libienz/se_2022_closet/startApp_1/clothes/editClothesFrag.java
+++ app/src/main/java/com/libienz/se_2022_closet/startApp_1/clothes/editClothesFrag.java
@@ -45,6 +45,7 @@ public class editClothesFrag extends Fragment {
     private StorageReference storageReference = storage.getReference().child("clothes").child(user.getUid());
     private String ClothesKey;
     private Uri imguri;
+    private Button editHashTag_btn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -133,6 +134,20 @@ public class editClothesFrag extends Fragment {
         });
 
         return view;
+
+
+
+
+
+        //태그 수정하기 버튼을 누르면 activiy_edit_hashtag 레이아웃으로 화면이 전환됨s
+        Button editHashTag_btn = (Button) view.findViewById(R.id.editHashTag_btn);
+        editHashTag_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //화면 전환
+            }
+        });
+
     }
 
     //파이어베이스 이미지 업로드 메소드
