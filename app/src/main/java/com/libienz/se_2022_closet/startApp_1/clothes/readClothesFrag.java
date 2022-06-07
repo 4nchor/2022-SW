c5b226d02d97c52664d3fea297a18acb42a1cf53
yejiniee <02doyoung01@naver.com>
2022-06-08 00:07:10 +0900

    merge

--- app/src/main/java/com/libienz/se_2022_closet/startApp_1/clothes/readClothesFrag.java
+++ app/src/main/java/com/libienz/se_2022_closet/startApp_1/clothes/readClothesFrag.java
@@ -116,7 +116,8 @@ public class readClothesFrag extends Fragment {
                 editClothesFrag.setArguments(bundle);
 
                 //열람 중이었던 의류를 수정하도록 함
-                getParentFragmentManager().beginTransaction().replace(R.id.frag_fl, editClothesFrag).addToBackStack(null).commit();
+                //frag_fl->editClothes_fg으로 id 수정
+                getParentFragmentManager().beginTransaction().replace(R.id.editClothes_fg, editClothesFrag).addToBackStack(null).commit();
             }
         });
 
