881472f64b55fc833cb0185a5ce8254ce48a493b
yejiniee <02doyoung01@naver.com>
2022-06-09 14:57:21 +0900

    리사이클러뷰 이미지  수정

--- app/src/main/java/com/libienz/se_2022_closet/startApp_1/clothes/ReadAllClothesFrag.java
+++ app/src/main/java/com/libienz/se_2022_closet/startApp_1/clothes/ReadAllClothesFrag.java
@@ -65,7 +65,6 @@ public class ReadAllClothesFrag extends Fragment {
 
 
 
-
         userRef.child(user.getUid()).child("Clothes").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
